6a8aae8aa4419cbe0adfc238b78b3537cb721472
Derek Foster <derekfoster94@gmail.com>
2021-02-27 16:34:23 -0800

    find and replace completed

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import { BsPencilSquare } from "react-icons/bs";
 import ListGroup from "react-bootstrap/ListGroup";
 import Spinner from "react-bootstrap/Spinner";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import { LinkContainer } from "react-router-bootstrap";
 import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
@@ -13,8 +15,15 @@ export default function Home() {
   const [notes, setNotes] = useState([]);
   const { isAuthenticated } = useAppContext();
   const [isLoading, setIsLoading] = useState(true);
+
+  //search
   const [searchString, setSearchString] = useState("");
   const [displayedNotes, setDisplayedNotes] = useState([]);
+
+  // find and replace
+  const [replaceModal, setReplaceModal] = useState(false);
+  const [findString, setFindString] = useState("");
+  const [replaceString, setReplaceString] = useState("");
 
   useEffect(() => {
     async function onLoad() {
@@ -46,6 +55,56 @@ export default function Home() {
     );
 
     setDisplayedNotes(filteredNotes);
+  }
+
+  function findAndReplace(find, replace) {
+    const replacedNotes = notes.map(note => {
+      note.content = note.content.replaceAll(find, replace);
+      return note;
+    });
+
+    setNotes(replacedNotes);
+    setReplaceModal(false);
+  }
+
+  function renderModal() {
+    return (
+      <Modal
+        show={() => setReplaceModal(true)}
+        onHide={() => setReplaceModal(false)}
+      >
+        <Modal.Header closeButton className="modal-header">
+          <Modal.Title>Find & Replace</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Find (or replace) all notes with any word or phrase. Be careful, there
+          is no "undo", and it is case sensitive!
+        </Modal.Body>
+        <input
+          className="replace-input"
+          type="text"
+          placeholder="Find"
+          onChange={e => setFindString(e.target.value)}
+        />
+        <input
+          className="replace-input"
+          type="text"
+          placeholder="Replace"
+          onChange={e => setReplaceString(e.target.value)}
+        />
+        <Modal.Footer>
+          <Button
+            variant="danger"
+            onClick={() => findAndReplace(findString, replaceString)}
+          >
+            Replace
+          </Button>
+          <Button variant="secondary" onClick={() => setReplaceModal(false)}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
   }
 
   function renderNotesList(notes) {
@@ -102,15 +161,23 @@ export default function Home() {
           <div className="notes">
             <h2 className="pb-3 mt-4 mb-3 border-bottom">Your Notes</h2>
             <ListGroup>{!isLoading && renderNotesList(notes)}</ListGroup>
-            <form className="search">
-              <input
-                type="text"
-                placeholder="Search..."
-                value={searchString}
-                onChange={e => setSearchString(e.target.value)}
-              />
-              <button onClick={e => searchNotes(e, searchString)}>Enter</button>
-            </form>
+            <div className="actions">
+              <form className="search">
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  value={searchString}
+                  onChange={e => setSearchString(e.target.value)}
+                />
+                <button onClick={e => searchNotes(e, searchString)}>
+                  Enter
+                </button>
+              </form>
+              <button onClick={() => setReplaceModal(true)}>
+                Find & Replace Text
+              </button>
+              {replaceModal && renderModal()}
+            </div>
           </div>
         )}
       </>
